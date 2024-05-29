1a48425c8b5aeaeec66197b696982c2dc5664a53
castrombithisamm <castro.mbithii@gmail.com>
2024-05-29 16:44:11 +0300

    refactor rendering logic for chat items and toggle icon

--- src/components/chat/chatSection/ChatSection.jsx
+++ src/components/chat/chatSection/ChatSection.jsx
@@ -1,47 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoMdArrowDown } from "react-icons/io";
 import Chats from "../chats/Chats";
-import { useState } from "react";
-import { GrAddCircle, GrSubtractCircle } from "react-icons/gr";
 
-export default function ChatSection({ month = "December ", chats = [] }) {
-  const [showChats, setShowChats] = useState(true);
-  return (
-    <div className="">
-      <div className="flex text-gray-400 my-4 justify-center items-center">
-        <div className="flex-grow">{month}</div>
-        <div
-          className=" cursor-pointer hover:text-orange-400"
-          onClick={() => setShowChats(!showChats)}>
-          {showChats ? (
-            <>
-              <IoMdArrowDown size={20} />
-            </>
-          ) : (
-            <>
-              <GrAddCircle size={20} />
-            </>
-          )}
-        </div>
-      </div>
-      {showChats ? (
-        <>
-          <div className=" flex flex-col gap-2">
-            {chats.map((chat, index) => {
-              return (
-                <Chats
-                  key={index}
-                  title={chat.title}
-                  quesNumbers={chat.numberOfQuestions}
-                  date={new Date(chat.timestamp)}
-                />
-              );
-            })}
-          </div>
-        </>
-      ) : (
-        <></>
-      )}
-    </div>
-  );
+export default function ChatSection({
+	monthYear = "",
+	threads = [],
+	onChatSelect = () => {},
+}) {
+	const [showChats, setShowChats] = useState(true);
+
+	const formatMonthYear = (key) => {
+		const [month, year] = key.split("-");
+		return `${new Date(year, month - 1).toLocaleString("en-us", {
+			month: "long",
+		})} ${year}`;
+	};
+
+	return (
+		<div className="">
+			<div className="flex text-gray-400 my-4 justify-center items-center">
+				<div className="flex-grow">{formatMonthYear(monthYear)}</div>
+				<div
+					className="cursor-pointer hover:text-orange-400"
+					onClick={() => setShowChats(!showChats)}
+				>
+					<IoMdArrowDown size={20} />
+				</div>
+			</div>
+			{showChats && (
+				<div className="flex flex-col gap-2">
+					{threads.map((thread, index) => (
+						<Chats
+							key={index}
+							id={thread.id}
+							title={thread.title}
+							quesNumbers={thread.messages.length}
+							date={new Date(thread.timestamp)}
+							onChatSelect={onChatSelect}
+						/>
+					))}
+				</div>
+			)}
+		</div>
+	);
 }
