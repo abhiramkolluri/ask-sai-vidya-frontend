1167851f789f70e3a5417bef91a1d24f0fd102e8
castrombithisamm <castro.mbithii@gmail.com>
2024-05-29 15:07:29 +0300

    Refactor state management to include thread handling

--- src/pages/chatpage/Chatpage.jsx
+++ src/pages/chatpage/Chatpage.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import Logo from "../../components/logo/Logo";
+import React, { useState, useEffect } from "react";
 import SideNav from "../../components/sidenav/SideNav";
 import ChatBox from "../../components/chatbox/ChatBox";
 import Login from "../../components/login/Login";
@@ -7,50 +6,78 @@ import Signup from "../../components/signup/Signup";
 import Navbar from "../../components/navbar/navbar";
 
 const Chatpage = () => {
-  const [newChat, setnewChat] = useState(Math.random());
-  const [showModal, setshowModal] = useState(false);
-  const [showlogin, setshowlogin] = useState(false);
-  const handleShowModal = () => {
-    console.log("show modal");
-    setshowModal(true);
-  };
-  const handleNewChat = () => {
-    setnewChat(Math.random());
-  };
-  return (
+	const [newChat, setNewChat] = useState(Math.random());
+	const [showModal, setShowModal] = useState(false);
+	const [showLogin, setShowLogin] = useState(false);
+	const [selectedThreadId, setSelectedThreadId] = useState(null);
+	const [threads, setThreads] = useState([]);
+
+	const handleShowModal = () => {
+		setShowModal(true);
+	};
+
+	const handleNewChat = () => {
+		const newThreadId = new Date().toISOString();
+		setNewChat(Math.random());
+		setSelectedThreadId(newThreadId);
+		addThread({
+			id: newThreadId,
+			title: "",
+			timestamp: new Date(),
+			messages: [],
+		});
+	};
+
+	const handleChatSelect = (threadId) => {
+		setSelectedThreadId(threadId);
+	};
+
+	const addThread = (thread) => {
+		setThreads((prevThreads) => {
+			const existingThread = prevThreads.find((t) => t.id === thread.id);
+			if (existingThread) {
+				return prevThreads.map((t) =>
+					t.id === thread.id ? { ...t, ...thread } : t
+				);
+			} else {
+				return [thread, ...prevThreads];
+			}
+		});
+	};
+	return (
     <div className="w-full h-[100vh] flex overflow-hidden ">
       <div className="w-[300px] bg-white shadow-lg  flex-col overflow-hidden hidden md:block md:flex flex-shrink-0">
         {/* this will hold the side nav section */}
         <SideNav startNewChatCallback={handleNewChat} />
-      </div>
-      <div className="absolute top-0 left-0 right-0">
-        <Navbar />
-      </div>
-      <div className="flex flex-col flex-grow mt-16">
+			</div>
+			<div className="absolute top-0 left-0 right-0">
+				<Navbar />
+			</div>
+			<div className="flex flex-col flex-grow mt-16">
         {/* this will hold the chat section */}
         <ChatBox newChat={newChat} modalCallback={() => handleShowModal()} />
-      </div>
+			</div>
       {showModal ? (
         <>
-          <div
-            className="absolute top-0 bottom-0 right-0 left-0 flex justify-center items-center bg-black bg-opacity-20 z-50"
+				<div
+					className="absolute top-0 bottom-0 right-0 left-0 flex justify-center items-center bg-black bg-opacity-20 z-50"
             onClick={handleShowModal}>
             {showlogin ? (
               <>
                 <Login callback={() => setshowlogin(false)} />
               </>
-            ) : (
+					) : (
               <>
                 <Signup callback={() => setshowlogin(true)} />
               </>
-            )}
-          </div>
+					)}
+				</div>
         </>
       ) : (
         <></>
-      )}
-    </div>
-  );
+			)}
+		</div>
+	);
 };
 
 export default Chatpage;
