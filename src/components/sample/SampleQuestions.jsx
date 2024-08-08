073e78e6c162fb58d0bbb9209d28bcecf38e01db
Abhiram Kolluri <abhiram.kolluri1@gmail.com>
2024-08-07 19:04:55 -0500

    New sample Questions

--- src/components/sample/SampleQuestions.jsx
+++ src/components/sample/SampleQuestions.jsx
@@ -4,16 +4,16 @@ export default function SampleQuestions() {
   return (
     <div className="w-full flex flex-wrap gap-4 text-gray-500 text-lg leading-6 cursor-pointer justify-center items-center">
       <div className="border rounded border-gray-300 hover:border-orange-500 hover:bg-orange-100 flex items-center px-4 py-2 w-[346px] transition-all ease-linear">
-        When is the good time to wake up?
+        How do you find life’s purpose?
       </div>
       <div className="border rounded border-gray-300 hover:border-orange-500 hover:bg-orange-100 flex items-center px-4 py-2 w-[346px] transition-all ease-linear">
-        When is the good time to go for walk?
+        In this ever changing world, how do I encourage my child to follow Sai Baba's values?
       </div>
       <div className="border rounded border-gray-300 hover:border-orange-500 hover:bg-orange-100 flex items-center px-4 py-2 w-[346px] transition-all ease-linear">
-        What is the defination?
+        What does Sai Baba say about stopping bad habits and learning new habits?
       </div>
       <div className="border rounded border-gray-300 hover:border-orange-500 hover:bg-orange-100 flex items-center px-4 py-2 w-[346px] transition-all ease-linear">
-        When is the good time to wake up?
+        How do I sing Bhajans, do service, learn prayers, meditate, and do yoga? 
       </div>
     </div>
   );
